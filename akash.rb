1ff9a0140d13180b41550495f94f2180438c076a
goreleaserbot <goreleaser@carlosbecker.com>
2019-05-16 11:09:22 -0700

    Brew formula update for akash version v0.4.3

--- akash.rb
+++ akash.rb
@@ -2,9 +2,9 @@
 class Akash < Formula
   desc "Blockchain-powered decentralized compute platform"
   homepage "https://akash.network"
-  url "https://github.com/ovrclk/akash/releases/download/v0.4.2/akash_0.4.2_darwin_amd64.tar.gz"
-  version "0.4.2"
-  sha256 "f17c25724361b2f05ab6f1b21935b0ee15b165659e84cfeefa21de5410efa964"
+  url "https://github.com/ovrclk/akash/releases/download/v0.4.3/akash_0.4.3_darwin_amd64.tar.gz"
+  version "0.4.3"
+  sha256 "a1e1770252122595811b96ddb21cefbe5f59f420feda2c04a40220c030bf0242"
 
   def install
     bin.install "akash"

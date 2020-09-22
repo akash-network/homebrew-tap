e646064c622a0ef90638374d602e58bbc4fe919d
goreleaserbot <goreleaser@carlosbecker.com>
2020-09-22 13:01:28 +0800

    Brew formula update for akash version v0.7.9-rc5

--- akash.rb
+++ akash.rb
@@ -2,28 +2,27 @@
 class Akash < Formula
   desc "Blockchain-powered decentralized compute platform"
   homepage "https://akash.network"
-  version "0.8.1"
+  version "0.7.9-rc5"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/ovrclk/akash/releases/download/v0.8.1/akash_0.8.1_darwin_amd64.zip"
-    sha256 "51e549933c902231c5c17aef5e8165b94c3da02c57ddbceb209a34b4c83e6016"
+    url "https://github.com/ovrclk/akash/releases/download/v0.7.9-rc5/akash_0.7.9-rc5_darwin_amd64.zip"
+    sha256 "60671af00d5b7e338e211bc54bec6594f94f3a5eee5836e43d1417c7546f2c6d"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/ovrclk/akash/releases/download/v0.8.1/akash_0.8.1_linux_amd64.zip"
-      sha256 "a4c55276d450cfde0415d5064f4662b2bf13477ba446d3427927ec33baed6e01"
+      url "https://github.com/ovrclk/akash/releases/download/v0.7.9-rc5/akash_0.7.9-rc5_linux_amd64.zip"
+      sha256 "1d4a8a029062847d80c61726e7f94bc41bc02fccb0fccd5c6f01fda3003776ae"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/ovrclk/akash/releases/download/v0.8.1/akash_0.8.1_linux_arm64.zip"
-        sha256 "d71febb8970cea12e8252e7dc21cec64153d72d7bded8f44e065a60e5764a2b3"
+        url "https://github.com/ovrclk/akash/releases/download/v0.7.9-rc5/akash_0.7.9-rc5_linux_arm64.zip"
+        sha256 "4b81da581034720d2ebfaf618e229c3e4777717ac8a81e30d6df200bf64f489a"
       else
       end
     end
   end
 
   def install
-    bin.install "akashctl"
-    bin.install "akashd"
+    bin.install "akash"
   end
 end

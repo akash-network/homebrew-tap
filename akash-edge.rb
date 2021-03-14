428d4782763f973e5c17c5fc9adafb04b42ee703
Atomzwieback <contact@atomzwieback.dev>
2021-03-14 22:44:49 +0100

    Update akash-edge.rb
    should fix the M1 issue?

--- akash-edge.rb
+++ akash-edge.rb
@@ -12,6 +12,10 @@ class AkashEdge < Formula
     url "https://github.com/ovrclk/akash/releases/download/v0.9.4-rc9/akash_0.9.4-rc9_darwin_amd64.zip"
     sha256 "d7ab692a8ec3d1f00638563779b920526211a012edb43436a0bcf1087e8ae327"
   end
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/ovrclk/akash/releases/download/v0.9.4-rc9/akash_0.9.4-rc9_linux_arm64.zip"
+    sha256 "7d04c83a7115a82a742d9acdae344f5bd44ab4f16d2261125cf2853a92ae3824"
+  end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/ovrclk/akash/releases/download/v0.9.4-rc9/akash_0.9.4-rc9_linux_amd64.zip"
     sha256 "a37fe58ed6ebd26aaf8e4621a6c355d5b9df5f1ef520f27b312fa9ec9451b0fd"

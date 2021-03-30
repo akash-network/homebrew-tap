ed5e007e8cd30500d827fe714bba2c7b5cd82531
goreleaserbot <goreleaser@carlosbecker.com>
2021-03-30 23:22:14 +0800

    Brew formula update for akash version v0.12.0

--- akash.rb
+++ akash.rb
@@ -5,24 +5,24 @@
 class Akash < Formula
   desc "Blockchain-powered decentralized compute platform"
   homepage "https://akash.network"
-  version "0.12.0-rc1"
+  version "0.12.0"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/ovrclk/akash/releases/download/v0.12.0-rc1/akash_0.12.0-rc1_darwin_amd64.zip"
-    sha256 "9f26a3f4e2e249a3c62fe6b7303e6aefc5e498f7067a66f7f7cede1f7c0d69aa"
+    url "https://github.com/ovrclk/akash/releases/download/v0.12.0/akash_0.12.0_darwin_amd64.zip"
+    sha256 "be2d5da7dddd0d06f9f70c7f7ac39e8ee29db0262526b5033f963688a62219b2"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/ovrclk/akash/releases/download/v0.12.0-rc1/akash_0.12.0-rc1_darwin_arm64.zip"
-    sha256 "ccc70703647c8384507b93e239e20fa88821f2e903aba369e07f08b3b5fb59d0"
+    url "https://github.com/ovrclk/akash/releases/download/v0.12.0/akash_0.12.0_darwin_arm64.zip"
+    sha256 "ef93bdcc0bb38c87d80f334449beeb6640181575c1abaeca0cc5aa0391f273c0"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/ovrclk/akash/releases/download/v0.12.0-rc1/akash_0.12.0-rc1_linux_amd64.zip"
-    sha256 "df623d19a903ca6a11ebd3901aa7fd5e556292ce26e67d2ee09a965e8a7acd86"
+    url "https://github.com/ovrclk/akash/releases/download/v0.12.0/akash_0.12.0_linux_amd64.zip"
+    sha256 "2bacdb2c01d414b6eee1dcf84081802380d1820bf06e6038b0cd255fcd0ae4b9"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/ovrclk/akash/releases/download/v0.12.0-rc1/akash_0.12.0-rc1_linux_arm64.zip"
-    sha256 "b2e4f4a03a7b53f67b3bd07bdfcf89f8709821276adf1f25bc27378c06fbb1e8"
+    url "https://github.com/ovrclk/akash/releases/download/v0.12.0/akash_0.12.0_linux_arm64.zip"
+    sha256 "4a12761a8496c6d1701be45f562ef6c6de74ef36bf35a19c8923036f25bc077b"
   end
 
 

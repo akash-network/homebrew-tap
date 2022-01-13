2fa982ae591ca87682fab1a5c4ba2e62bfae2a57
goreleaserbot <goreleaser@carlosbecker.com>
2022-01-13 12:13:16 -0800

    Brew formula update for akash version v0.15.0-rc6

--- akash-edge.rb
+++ akash-edge.rb
@@ -5,24 +5,24 @@
 class AkashEdge < Formula
   desc "Blockchain-powered decentralized compute platform"
   homepage "https://akash.network"
-  version "0.15.0-rc5"
+  version "0.15.0-rc6"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/ovrclk/akash/releases/download/v0.15.0-rc5/akash_0.15.0-rc5_darwin_amd64.zip"
-    sha256 "d812557c6cec45915f8aa88f2b06516f7de4e9e3e7bfa3055efaddb5ccfcdaff"
+    url "https://github.com/ovrclk/akash/releases/download/v0.15.0-rc6/akash_0.15.0-rc6_darwin_amd64.zip"
+    sha256 "55bae241bb7362a300399ca95f38e329e488db3b31fe95d3890d8be3da0b5905"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/ovrclk/akash/releases/download/v0.15.0-rc5/akash_0.15.0-rc5_darwin_arm64.zip"
-    sha256 "1b1c958d8f6d6384700b140a27fca117aeb62da10e815362b2e5061c86a0fe5f"
+    url "https://github.com/ovrclk/akash/releases/download/v0.15.0-rc6/akash_0.15.0-rc6_darwin_arm64.zip"
+    sha256 "a7bac6ac683bb88b7ade8bcd310950f456e840ae39573f7c8321d9ba91873abd"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/ovrclk/akash/releases/download/v0.15.0-rc5/akash_0.15.0-rc5_linux_amd64.zip"
-    sha256 "f89af9e66afc0e9373721f970bf5feabe7fe56c0df9f13cdf9b5ae2fe83be049"
+    url "https://github.com/ovrclk/akash/releases/download/v0.15.0-rc6/akash_0.15.0-rc6_linux_amd64.zip"
+    sha256 "d17fedb34e4b337c609c8fa52a6078bf7abe3446ecf134d304c776bb84b8a3fb"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/ovrclk/akash/releases/download/v0.15.0-rc5/akash_0.15.0-rc5_linux_arm64.zip"
-    sha256 "6a40c357de08d7493211f9a93e65a51150d35c3e920111ee480faae1a0d24de8"
+    url "https://github.com/ovrclk/akash/releases/download/v0.15.0-rc6/akash_0.15.0-rc6_linux_arm64.zip"
+    sha256 "6bde8ae3343013784ea03982c15eb6ff24e03e1fc747a47f6f551409ef05626f"
   end
 
   keg_only :unneeded, "This is testnet release. Run brew install ovrclk/tap/akash to install mainnet version"

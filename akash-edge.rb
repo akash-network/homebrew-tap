6cf75d72e0994276c99c7de6e2fcc4e168b8badc
goreleaserbot <goreleaser@carlosbecker.com>
2022-03-08 13:41:24 -0800

    Brew formula update for akash version v0.15.0-rc15

--- akash-edge.rb
+++ akash-edge.rb
@@ -5,20 +5,20 @@
 class AkashEdge < Formula
   desc "Blockchain-powered decentralized compute platform"
   homepage "https://akash.network"
-  version "0.15.0-rc14"
+  version "0.15.0-rc15"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ovrclk/akash/releases/download/v0.15.0-rc14/akash_0.15.0-rc14_darwin_arm64.zip"
-      sha256 "06c6c3d642d835a6902656ce8dc51514c36e229f1761f6e5d6e7e93473f61465"
+      url "https://github.com/ovrclk/akash/releases/download/v0.15.0-rc15/akash_0.15.0-rc15_darwin_arm64.zip"
+      sha256 "d962cb4f144b20b065b6ef540ba44cf2c7079c4ef3d9514418b695019c133a37"
 
       def install
         bin.install "akash"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/ovrclk/akash/releases/download/v0.15.0-rc14/akash_0.15.0-rc14_darwin_amd64.zip"
-      sha256 "986f5b7172647109a9cfa55c36502717ff0b4f7d97bd1d0cc465e0a3ba772fb8"
+      url "https://github.com/ovrclk/akash/releases/download/v0.15.0-rc15/akash_0.15.0-rc15_darwin_amd64.zip"
+      sha256 "f760bab27de04d3760abae1b6a382a733df82ae8f1ab518cdfdae0526f57746b"
 
       def install
         bin.install "akash"
@@ -27,17 +27,17 @@ class AkashEdge < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/ovrclk/akash/releases/download/v0.15.0-rc14/akash_0.15.0-rc14_linux_arm64.zip"
-      sha256 "25fef05b5961d44ea3dffd11166c5b6f35425b4cfc89b2f4362a49e6c9c5a902"
+    if Hardware::CPU.intel?
+      url "https://github.com/ovrclk/akash/releases/download/v0.15.0-rc15/akash_0.15.0-rc15_linux_amd64.zip"
+      sha256 "ffc60a1ae583005ea22273be0675949247915d2b2253d4942d2c04677a3c7310"
 
       def install
         bin.install "akash"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/ovrclk/akash/releases/download/v0.15.0-rc14/akash_0.15.0-rc14_linux_amd64.zip"
-      sha256 "5a51879da716ae79aa972f7e70604688567ad7d2105f2e7fc8dbc8b1686ff171"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/ovrclk/akash/releases/download/v0.15.0-rc15/akash_0.15.0-rc15_linux_arm64.zip"
+      sha256 "0ee748bb26a39eb286825e77fd9685d8819ed8abf0c3a7a76936d60b1fe23d77"
 
       def install
         bin.install "akash"

dcbf999dd438f3986f88d9aaf526e79d05dea047
goreleaserbot <goreleaser@carlosbecker.com>
2022-05-12 13:53:25 -0700

    Brew formula update for akash version v0.16.4-rc1

--- akash-test.rb
+++ akash-test.rb
@@ -5,20 +5,20 @@
 class AkashTest < Formula
   desc "Blockchain-powered decentralized compute platform"
   homepage "https://akash.network"
-  version "0.16.4-rc0"
+  version "0.16.4-rc1"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/ovrclk/akash/releases/download/v0.16.4-rc0/akash_0.16.4-rc0_darwin_arm64.zip"
-      sha256 "94a049575e7e0c82872bcd958f43aa34881579a21d0de421cfe5e931da9f2128"
+      url "https://github.com/ovrclk/akash/releases/download/v0.16.4-rc1/akash_0.16.4-rc1_darwin_arm64.zip"
+      sha256 "1c59d310ea447c9a670af8fefbbff6f8eec480e7463f315feaa22b5d056e5740"
 
       def install
         bin.install "akash"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/ovrclk/akash/releases/download/v0.16.4-rc0/akash_0.16.4-rc0_darwin_amd64.zip"
-      sha256 "a4f564ffab78f1c9395167579f79ce0fbe24e2f6f3f52394a9928718d318b211"
+      url "https://github.com/ovrclk/akash/releases/download/v0.16.4-rc1/akash_0.16.4-rc1_darwin_amd64.zip"
+      sha256 "2aaecc1c327ac77f52c27f50118b30acb11ec7622a6ba7203d174368d99fb6ef"
 
       def install
         bin.install "akash"
@@ -28,16 +28,16 @@ class AkashTest < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/ovrclk/akash/releases/download/v0.16.4-rc0/akash_0.16.4-rc0_linux_arm64.zip"
-      sha256 "e441cd82c79ead521a957e7cf0d127ff26e0876219e46a0c03ff42686fbdc62f"
+      url "https://github.com/ovrclk/akash/releases/download/v0.16.4-rc1/akash_0.16.4-rc1_linux_arm64.zip"
+      sha256 "2133bedd5b6d34a28480d54f3c140d90766b12357289def15b110803751d9e47"
 
       def install
         bin.install "akash"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/ovrclk/akash/releases/download/v0.16.4-rc0/akash_0.16.4-rc0_linux_amd64.zip"
-      sha256 "5f9ce47142b1d63bb59b96dfc6c503def4b3cef73b0f1d1fed641e352ceb3459"
+      url "https://github.com/ovrclk/akash/releases/download/v0.16.4-rc1/akash_0.16.4-rc1_linux_amd64.zip"
+      sha256 "218d34bf07ac57f17b873d131359b315fe7a8a6a574468e905e805aa227136ad"
 
       def install
         bin.install "akash"

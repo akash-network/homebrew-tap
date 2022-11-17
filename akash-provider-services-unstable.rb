14c341a348cc987d5057cc56ffa224f846463665
troian <troian@users.noreply.github.com>
2022-11-17 01:40:23 +0000

    feat: akash-provider-services-unstable.rb v0.1.0-rc16

--- akash-provider-services-unstable.rb
+++ akash-provider-services-unstable.rb
@@ -2,11 +2,11 @@
 class AKashProviderServicesUnstable < Formula
   desc "Blockchain-powered decentralized compute platform"
   homepage "https://akash.network"
-  version "0.1.0-rc15"
+  version "0.1.0-rc16"
 
   on_macos do
-    url "https://github.com/ovrclk/akash/releases/download/v0.1.0-rc15/provider-services_darwin_all.zip"
-    sha256 "b3029d895610bca609bdd45fb22ba0ff30f733d9335cb5df089fc911af443472"
+    url "https://github.com/ovrclk/akash/releases/download/v0.1.0-rc16/provider-services_darwin_all.zip"
+    sha256 "b71d843d8c04510995d1b78c399002ca463641fea6c15fb0fb5f4dd8fe2d6b8f"
 
     def install
       bin.install "provider-services"
@@ -15,16 +15,16 @@ class AKashProviderServicesUnstable < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/ovrclk/akash/releases/download/v0.1.0-rc15/akash_linux_arm64.zip"
-      sha256 "84c19c8cbd414de3185cb1ebd7463745389b86bf4187870f08d954f72ac621f3"
+      url "https://github.com/ovrclk/akash/releases/download/v0.1.0-rc16/akash_linux_arm64.zip"
+      sha256 "eb0fa8542d7243f59ef008a5dc01efff9b0d56f73594e3c4da1b4cb46ee42489"
 
       def install
         bin.install "provider-services"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/ovrclk/akash/releases/download/v0.1.0-rc15/akash_linux_amd64.zip"
-      sha256 "55b7bac52fd97c5c70011657335b1111da639266e8217ce859dc63de430f8f03"
+      url "https://github.com/ovrclk/akash/releases/download/v0.1.0-rc16/akash_linux_amd64.zip"
+      sha256 "bd2d7dcb616e592b5955b0905653266610916c2086727f45fd6fc1d1087f0be8"
 
       def install
         bin.install "provider-services"

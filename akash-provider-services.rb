63a1b4d87898f7ba4176dbcfd80964616acf5369
Artur Troian <troian.ap@gmail.com>
2023-09-06 10:56:52 -0400

    fix(provider-services): update checksums

--- akash-provider-services.rb
+++ akash-provider-services.rb
@@ -6,7 +6,7 @@ class AkashProviderServices < Formula
 
   on_macos do
     url "https://github.com/akash-network/provider/releases/download/v0.4.6/provider-services_darwin_all.zip"
-    sha256 "e8c223d632050146094efd9596e6d404a888f07e0ab3d0dfaad10566ea7533a6"
+    sha256 "bf0119141565e4923139341704daf1c1786cea9fada441c7b7f4b46c2f4e0b63"
 
     def install
       bin.install "provider-services"
@@ -16,7 +16,7 @@ class AkashProviderServices < Formula
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/akash-network/provider/releases/download/v0.4.6/provider-services_linux_arm64.zip"
-      sha256 "aa22df049661c2d098c2c95566b9168e48fc369157d3321be3b9b144cdbdeb9a"
+      sha256 "b2f76ad02df6388e5b7bb71df3218c8848089a2cf551a1b76489ccb298bfe390"
 
       def install
         bin.install "provider-services"
@@ -24,7 +24,7 @@ class AkashProviderServices < Formula
     end
     if Hardware::CPU.intel?
       url "https://github.com/akash-network/provider/releases/download/v0.4.6/provider-services_linux_amd64.zip"
-      sha256 "1b1996096d9a2e0d6e8e70a52a9d8ec57198c32c27533331973f4e84a4d77690"
+      sha256 "9e49b1eb8365751cf599292ab989ecfaf34a03c737de83f44d6fa4cdae32b729"
 
       def install
         bin.install "provider-services"
